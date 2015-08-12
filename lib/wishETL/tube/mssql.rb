84a010948ae46a64ecc762031841b7fb8b8ece46
John W Higgins <wishdev@gmail.com>
2015-08-12 16:02:35 -0700

    Add port env variable

--- lib/wishETL/tube/mssql.rb
+++ lib/wishETL/tube/mssql.rb
@@ -4,7 +4,8 @@ class MSSQLHelper
   attr_reader :res
 
   def initialize(opts = {})
-    @conn = TinyTds::Client.new(:username => ENV['MSSQLUSER'], :password => ENV['MSSQLPASS'], :host => ENV['MSSQLHOST'], :timeout => 60000)
+    ENV['MSSQLPORT'] ||= 1433
+    @conn = TinyTds::Client.new(:username => ENV['MSSQLUSER'], :password => ENV['MSSQLPASS'], :host => ENV['MSSQLHOST'], :port => ENV['MSSQLPORT'], :timeout => 60000)
   end
 
   def exec(sql)
